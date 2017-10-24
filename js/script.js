f62678e1e610bdca485c1c6f407c29b2e483fffd
Niek <n.eeftink@gmail.com>
2017-10-24 09:07:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.body.style.background = '#404040';
-document.body.style.backgroundImage = 'url(https://unsplash.it/' + window.innerWidth + '/' + window.innerHeight + '/?random&blur)';
+document.body.style.backgroundImage = 'url(https://unsplash.it/' + window.innerWidth + '/' + window.innerHeight + '/?random)';
 
 var searchbox = document.getElementById('search');
 var searchbutton = document.getElementById('button-search')
